588efcc2067bd55cf7f92cc6ba48a92b8a0a06f7
R3shotka <148250319+R3shotka@users.noreply.github.com>
2024-12-15 14:18:43 +0200

    Create UserController.cs

---
+++ code/UserController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserService _userService;
+
+        public UserController(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<User>>> GetAllUsers()
+        {
+            var users = await _userService.GetAllUsersAsync();
+            return Ok(users);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<User>> CreateUser(User user)
+        {
+            var createdUser = await _userService.CreateUserAsync(user);
+            return CreatedAtAction(nameof(GetAllUsers), new { id = createdUser.Id }, createdUser);
+        }
+    }
+}
